9b4e114b4431ce12fc2c20e15fc1d7f5416fc5ee
Jesús Navarrete <jesus.navarrete@gmail.com>
2011-11-27 13:53:24 +0100

    first test of the compiler

---
+++ test/cradle_test.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+require 'test/unit'
+
+class CradleTest < Test::Unit::TestCase
+  def test_init
+    assert false
+  end
+
+  def test_get_char
+    assert false
+  end
+
+  def test_error(text)
+    assert false
+  end
+
+  def test_expected(text)
+    assert false
+  end
+
+  def test_match(input)
+    assert false
+  end
+
+  def test_is_digit?(input)
+    assert false
+  end
+
+  def test_is_alpha?(input)
+    assert false
+  end
+
+  def test_get_name
+    assert false
+  end
+
+  def test_get_num
+    assert false
+  end
+
+  def test_emit(input)
+    assert false
+  end
+
+  def test_emit_ln(input)
+    assert false
+  end
+
+  ## Chapter II
+  def test_expression
+    assert false
+  end
+end
+  
